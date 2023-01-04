9b5dd5cf6d90043bb434841c126b21c3f8fcd922
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-01-04 23:01:09 +0700

    feat: create 'best_offer copy.dart' and best_offer_item_widget.dart

---
+++ lib/screen/home/widget/best_offer copy.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_house_rent/model/house.dart';
+
+class BestOffer extends StatelessWidget {
+  final listOffer = House.generateBestOffer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Best Offer',
+                style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  'See All',
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/home/widget/best_offer_item_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_house_rent/model/house.dart';
+
+class BestOfferItemWidget extends StatelessWidget {
+  House house;
+  BestOfferItemWidget({
+    Key? key,
+    required this.house,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Stack(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 150,
+                height: 80,
+                decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage(house.imageUrl), fit: BoxFit.cover), borderRadius: BorderRadius.circular(8)),
+              ),
+              const SizedBox(
+                width: 10.0,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    house.name,
+                    style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    house.address,
+                    style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 12.0),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
