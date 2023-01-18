dcd9b6cc27547d4e40baa148ac1261c8f617263a
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-01-18 22:10:27 +0700

    update main.dart, pubspec.lock and pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_house_rent/screen/home/home_screen.dart';
 import 'package:flutter_house_rent/theme.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,23 +13,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Rent House APP',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        backgroundColor: backgroundColor,
-        primaryColor: primaryColor,
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          secondary: accentColor,
-        ),
-        textTheme: TextTheme(
-          headline1: TextStyle(color: headline1Color),
-          bodyText1: TextStyle(color: bodyText1Color),
-        ),
-      ),
-      home: Scaffold(
-        body: HomeScreen(),
-      ),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          title: 'Flutter Rent House APP',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            backgroundColor: backgroundColor,
+            primaryColor: primaryColor,
+            colorScheme: ColorScheme.fromSwatch().copyWith(
+              secondary: accentColor,
+            ),
+            textTheme: TextTheme(
+              headline1: TextStyle(color: headline1Color),
+              bodyText1: TextStyle(color: bodyText1Color),
+            ),
+          ),
+          home: HomeScreen(),
+        );
+      },
     );
   }
 }
